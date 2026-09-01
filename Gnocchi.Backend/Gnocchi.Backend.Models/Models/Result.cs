d4baf88a6656860e144adac072f2210dde6082e2
SunberryBlossom <codemonke.lethargy455@passmail.com>
2026-09-01 16:42:00 +0200

    fix!(models): Change Delete Behaviour
    Due to a racing cascading condition, delete behaviour for the Result
    table cannot be cascade on both of the relations (to cookingmethod and
    ingredient).
    
    In order to get around this, I have set both to No Action for now and
    added a TODO to look into this (if I have time).

--- Gnocchi.Backend/Gnocchi.Backend.Models/Models/Result.cs
+++ Gnocchi.Backend/Gnocchi.Backend.Models/Models/Result.cs
@@ -5,6 +5,7 @@ namespace Gnocchi.Backend.Models;
 
 public class Result
 {
+    // TODO: The delete behaviours are NOT optimised here... Both Cannot be cascading due to race issues.
     #region Properties
     public Guid Id { get; init; }
     [Required]
@@ -14,12 +15,12 @@ public class Result
     [ForeignKey(nameof(Ingredient))]
     public Guid IngredientId { get; set; }
     [Required]
-    [DeleteBehavior(DeleteBehavior.Cascade)]
+    [DeleteBehavior(DeleteBehavior.NoAction)]
     public Ingredient? Ingredient { get; set; }
     [ForeignKey(nameof(CookingMethod))]
     public Guid CookingMethodId { get; set; }
     [Required]
-    [DeleteBehavior(DeleteBehavior.Cascade)]
+    [DeleteBehavior(DeleteBehavior.NoAction)]
     public CookingMethod? CookingMethod { get; set; }
     public ICollection<RecipeStep>? RecipeSteps { get; set; }
     #endregion
