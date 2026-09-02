d2ede25b9711fd7038b1171baad1adab7d98bdd5
SunberryBlossom <codemonke.lethargy455@passmail.com>
2026-09-02 13:07:47 +0200

    feat(interfaces): add complete CRUD signatures to Cooking method interface

--- Gnocchi.Backend/Gnocchi.Backend.BLL/Interfaces/ICookingMethodRepository.cs
+++ Gnocchi.Backend/Gnocchi.Backend.BLL/Interfaces/ICookingMethodRepository.cs
@@ -6,10 +6,14 @@ public interface ICookingMethodRepository
     public Task AddAsync(CookingMethod cookingMethod, CancellationToken ct = default);
     #endregion
     #region Read signatures
-    public Task<CookingMethod?> GetWithMethodAsync(Guid id, CancellationToken ct = default);
+    public Task<IReadOnlyList<CookingMethod>> GetAllAsync(CancellationToken ct = default);
+    public Task<CookingMethod?> GetAsync(Guid id, CancellationToken ct = default);
+    public Task<CookingMethod?> GetFullAsync(Guid id, CancellationToken ct = default);
+    public Task<CookingMethod?> GetWithResultsAsync(Guid id, CancellationToken ct = default);
+    public Task<CookingMethod?> GetWithScoreAsync(Guid id, CancellationToken ct = default);
     #endregion
     #region Update signatures
-    public Task<CookingMethod?> UpdateScoreAsync(Guid cookingMethodId, Guid scoreId, CancellationToken ct = default);
+    public Task<CookingMethod?> UpdateScoreAsync(Guid cookingMethodId, Guid newScoreId, CancellationToken ct = default);
     #endregion
     #region Delete signatures
     public Task DeleteAsync(Guid id, CancellationToken ct = default);
