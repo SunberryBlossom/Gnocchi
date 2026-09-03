776392813c4c191aee49a0c193de84c7302aa331
SunberryBlossom <codemonke.lethargy455@passmail.com>
2026-09-03 16:09:59 +0200

    fix!(Variant repository interface): change add and remove signatures to synchronous, fix typos

--- Gnocchi.Backend/Gnocchi.Backend.BLL/Interfaces/IVariantRepository.cs
+++ Gnocchi.Backend/Gnocchi.Backend.BLL/Interfaces/IVariantRepository.cs
@@ -3,17 +3,17 @@ namespace Gnocchi.Backend.Bll.Interfaces;
 public interface IVariantRepository
 {
     #region Create signatures
-    public Task AddAsync(Variant variant, CancellationToken ct = default);
+    public void Add(Variant variant);
     #endregion
     #region Read signatures
     public Task<IReadOnlyList<Variant>> GetAllAsync(CancellationToken ct = default);
-    public Task<Result?> GetAsync(string id, CancellationToken ct = default);
-    public Task<Result?> GetWithDishesAsync(string id, CancellationToken ct = default);
+    public Task<Variant?> GetAsync(string id, CancellationToken ct = default);
+    public Task<Variant?> GetWithDishesAsync(string id, CancellationToken ct = default);
     #endregion
     #region Update signatures
     // None! If you update the enum field, you are essentially creating a new Variant, not updating it.
     #endregion
     #region Delete signatures
-    public Task DeleteAsync(string id, CancellationToken ct = default);
+    public void Remove(Variant variant);
     #endregion
 }
