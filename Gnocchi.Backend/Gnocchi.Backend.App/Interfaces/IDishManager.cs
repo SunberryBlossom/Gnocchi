962eaad06cbf63f99297024720087542040c0bba
SunberryBlossom <codemonke.lethargy455@passmail.com>
2026-09-03 17:54:07 +0200

    build(App): add interface for DishManager

---
+++ Gnocchi.Backend/Gnocchi.Backend.App/Interfaces/IDishManager.cs
@@ -0,0 +1,21 @@
+using Gnocchi.Backend.Models;
+namespace Gnocchi.Backend.App.Services;
+
+public interface IDishManager
+{
+    #region Create signatures
+    public Task AddAsync(Dish dish, CancellationToken ct = default);
+    #endregion
+    #region Read signatures
+    public Task<Dish> GetByIdAsync(string id, CancellationToken ct = default);
+    public Task<Dish> GetCompleteByIdAsync(string id, CancellationToken ct = default);
+    public Task<Dish> GetWithSpecificEntityAsync(string id, string entity, CancellationToken ct = default);
+    #endregion
+    #region Update signatures
+    public Task<Dish> UpdateAsync(string id, string attribute, string newValue, CancellationToken ct = default);
+    #endregion
+    #region Delete signatures
+    public Task RemoveAsync(Dish dish);
+    #endregion
+
+}
