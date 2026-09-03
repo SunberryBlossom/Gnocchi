0548e9e5bdc16506095231bc0b5a20d759956a9e
SunberryBlossom <codemonke.lethargy455@passmail.com>
2026-09-03 16:10:09 +0200

    feat!(DAL): create Variant repository

---
+++ Gnocchi.Backend/Gnocchi.Backend.DAL/Repositories/VariantRepository.cs
@@ -0,0 +1,50 @@
+using Gnocchi.Backend.Bll.Interfaces;
+using Gnocchi.Backend.Models;
+using Microsoft.EntityFrameworkCore;
+
+namespace Gnocchi.Backend.DAL.Repositories;
+
+public class VariantRepository : IVariantRepository
+{
+    #region Fields
+    private readonly GnocchiDbContext _dbContext;
+    #endregion
+    #region Constructors
+    public VariantRepository(GnocchiDbContext dbContext)
+    {
+        _dbContext = dbContext;
+    }
+    #endregion
+    #region Create methods
+    public void Add(Variant variant)
+    {
+        _dbContext.Variants.Add(variant);
+    }
+    #endregion
+    #region Read methods
+    public async Task<IReadOnlyList<Variant>> GetAllAsync(CancellationToken ct = default)
+    {
+        return await _dbContext.Variants.ToListAsync(ct);
+    }
+    public async Task<Variant?> GetAsync(string id, CancellationToken ct = default)
+    {
+        return await _dbContext.Variants.FindAsync(id, ct);
+    }
+    public async Task<Variant?> GetWithDishesAsync(string id, CancellationToken ct = default)
+    {
+        return await _dbContext.Variants
+        .Where(variant => variant.VariantId == id)
+        .Include(variant => variant.Dishes)
+        .AsNoTracking()
+        .FirstOrDefaultAsync(ct);
+    }
+    #endregion
+    #region Update methods
+    #endregion
+    #region Delete methods
+    public void Remove(Variant variant)
+    {
+        _dbContext.Variants.Remove(variant);
+    }
+    #endregion
+}
