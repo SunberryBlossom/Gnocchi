bee711738af28bbe4e45690e6b12d0dea407b2f8
SunberryBlossom <codemonke.lethargy455@passmail.com>
2026-09-03 15:02:57 +0200

    fix!(Score repository interface): change add and remove signatures to synchronous

--- Gnocchi.Backend/Gnocchi.Backend.BLL/Interfaces/IScoreRepository.cs
+++ Gnocchi.Backend/Gnocchi.Backend.BLL/Interfaces/IScoreRepository.cs
@@ -3,13 +3,13 @@ namespace Gnocchi.Backend.Bll.Interfaces;
 public interface IScoreRepository
 {
     #region Create signatures
-    public Task AddAsync(Score score, CancellationToken ct = default);
+    public void Add(Score score);
     #endregion
     #region Read signatures
     public Task<Score?> GetFullAsync(string id, CancellationToken ct = default);
     public Task<Score?> GetWithIngredientsAsync(string id, CancellationToken ct = default);
     public Task<Score?> GetWithCookingMethodAsync(string id, CancellationToken ct = default);
-    public Task<Score> GetWithDishesAsync(string id, CancellationToken ct = default);
+    public Task<Score?> GetWithDishesAsync(string id, CancellationToken ct = default);
     public Task<Score?> GetAsync(string id, CancellationToken ct = default);
     public Task<IReadOnlyList<Score>> GetAllAsync(CancellationToken ct = default);
     #endregion
@@ -17,6 +17,6 @@ public interface IScoreRepository
     // None! If a score is not to satisfaction, delete it and add a new one.
     #endregion
     #region Delete signatures
-    public Task DeleteAsync(string id, CancellationToken ct = default);
+    public void Remove(Score score);
     #endregion
 }
