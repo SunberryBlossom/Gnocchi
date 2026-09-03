ea0a0017d3824b7a853a85618ca572818b5e83e3
SunberryBlossom <codemonke.lethargy455@passmail.com>
2026-09-03 15:57:05 +0200

    feat!(BLL): remove generic read method signature from user interface
    At this point, I cannot really crack how to use the params to
    dynamically write a method that would use Include queries through LINQ,
    if it even is possible. Since I do not have infinite time with this
    project, I have chosen to remove it from the interface implementation
    for now.
    
    Also made Add and Remove synchronous methods

--- Gnocchi.Backend/Gnocchi.Backend.BLL/Interfaces/IUserRepository.cs
+++ Gnocchi.Backend/Gnocchi.Backend.BLL/Interfaces/IUserRepository.cs
@@ -3,18 +3,17 @@ namespace Gnocchi.Backend.Bll.Interfaces;
 public interface IUserRepository
 {
     #region Create signatures
-    public Task AddAsync(User user, CancellationToken ct = default);
+    public void Add(User user);
     #endregion
     #region Read signatures
     public Task<IReadOnlyList<User>> GetAllAsync(CancellationToken ct = default);
     public Task<User?> GetAsync(string id, CancellationToken ct = default);
     public Task<User?> GetFullAsync(string id, CancellationToken ct = default);
-    public Task<User?> GetWithSpecificEntitiesAsync(string id, CancellationToken ct = default, params int[] entities);
     #endregion
     #region Update signatures
     // None as of now; Identity most likely handles this, and if you wish to update another entity for a user, you should use that specific repository.
     #endregion
     #region Delete signatures
-    public Task DeleteAsync(string id, CancellationToken ct = default);
+    public void Remove(User user);
     #endregion
 }
