dafe211621ca9fff677f2632fa5aaf4637173802
SunberryBlossom <codemonke.lethargy455@passmail.com>
2026-09-04 13:05:28 +0200

    fix!(Dish manager interface): change return value to nullable typing

--- Gnocchi.Backend/Gnocchi.Backend.App/Interfaces/IDishManager.cs
+++ Gnocchi.Backend/Gnocchi.Backend.App/Interfaces/IDishManager.cs
@@ -7,12 +7,12 @@ public interface IDishManager
     public Task AddAsync(Dish dish, CancellationToken ct = default);
     #endregion
     #region Read signatures
-    public Task<Dish> GetByIdAsync(string id, CancellationToken ct = default);
-    public Task<Dish> GetCompleteByIdAsync(string id, CancellationToken ct = default);
-    public Task<Dish> GetWithSpecificEntityAsync(string id, string entity, CancellationToken ct = default);
+    public Task<Dish?> GetByIdAsync(string id, CancellationToken ct = default);
+    public Task<Dish?> GetCompleteByIdAsync(string id, CancellationToken ct = default);
+    public Task<Dish?> GetWithSpecificEntityAsync(string id, string entity, CancellationToken ct = default);
     #endregion
     #region Update signatures
-    public Task<Dish> UpdateAsync(string id, string attribute, string newValue, CancellationToken ct = default);
+    public Task<Dish?> UpdateAsync(string id, string attribute, string newValue, CancellationToken ct = default);
     #endregion
     #region Delete signatures
     public Task RemoveAsync(Dish dish);
