024e72c417491c893ec2c1af40ac9dbed27863c9
SunberryBlossom <codemonke.lethargy455@passmail.com>
2026-09-04 13:06:13 +0200

    feat!(BLL) implement DishManager

--- Gnocchi.Backend/Gnocchi.Backend.BLL/Managers/DishManager.cs
+++ Gnocchi.Backend/Gnocchi.Backend.BLL/Managers/DishManager.cs
@@ -1,44 +1,66 @@
 using Gnocchi.Backend.App.Services;
 using Gnocchi.Backend.BLL.Interfaces;
-namespace Gnocchi.Backend.BLL.Services;
+namespace Gnocchi.Backend.BLL.Managers;
 
 public class DishManager : IDishManager
 {
+    #region Fields
     private readonly IDishRepository _dishRepository;
     private readonly IUnitOfWork _unitOfWork;
+    #endregion
+    #region Constructors
     public DishManager(IDishRepository dishRepository, IUnitOfWork unitOfWork)
     {
         _dishRepository = dishRepository;
         _unitOfWork = unitOfWork;
     }
+    #endregion
+    #region Create methods
     public async Task AddAsync(Dish dish, CancellationToken ct = default)
     {
         _dishRepository.Add(dish);
         await _unitOfWork.SaveChangesAsync();
     }
-
-    public Task<Dish> GetByIdAsync(string id, CancellationToken ct = default)
+    #endregion
+    #region Read methods
+    public async Task<Dish?> GetByIdAsync(string id, CancellationToken ct = default)
     {
-        throw new NotImplementedException();
+        return await _dishRepository.GetAsync(id, ct);
     }
-
-    public Task<Dish> GetCompleteByIdAsync(string id, CancellationToken ct = default)
+    public async Task<Dish?> GetCompleteByIdAsync(string id, CancellationToken ct = default)
     {
-        throw new NotImplementedException();
+        return await _dishRepository.GetFullAsync(id, ct);
     }
-
-    public Task<Dish> GetWithSpecificEntityAsync(string id, string entity, CancellationToken ct = default)
+    public async Task<Dish?> GetWithSpecificEntityAsync(string id, string entity, CancellationToken ct = default)
     {
-        throw new NotImplementedException();
+        return entity switch
+        {
+            "score" => await _dishRepository.GetWithScoreAsync(id, ct),
+            "recipesteps" => await _dishRepository.GetwithRecipeStepsAsync(id, ct),
+            "variant" => await _dishRepository.GetWithVariantAsync(id, ct),
+            _ => null
+        };
     }
-
-    public Task RemoveAsync(Dish dish)
+    #endregion
+    #region Update methods
+    public async Task<Dish?> UpdateAsync(string id, string attribute, string newValue, CancellationToken ct = default)
     {
-        throw new NotImplementedException();
+        var result = attribute switch
+        {
+            "name" => await _dishRepository.UpdateNameAsync(id, newValue, ct),
+            "variant" => await _dishRepository.UpdateVariantAsync(id, newValue, ct),
+            "score" => await _dishRepository.UpdateScoreAsync(id, newValue, ct),
+            _ => null
+        };
+        await _unitOfWork.SaveChangesAsync();
+        return result;
     }
-
-    public Task<Dish> UpdateAsync(string id, string attribute, string newValue, CancellationToken ct = default)
+    #endregion
+    #region Delete methods
+    public async Task RemoveAsync(Dish dish)
     {
-        throw new NotImplementedException();
+        _dishRepository.Remove(dish);
+        await _unitOfWork.SaveChangesAsync();
     }
+    #endregion
 }
