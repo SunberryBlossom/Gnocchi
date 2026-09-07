c9582e8cc14f888eb468d6f0b4813e9ef1bacd50
SunberryBlossom <codemonke.lethargy455@passmail.com>
2026-09-07 15:45:40 +0200

    feat(App): add VariantService

---
+++ Gnocchi.Backend/Gnocchi.Backend.App/Services/VariantService.cs
@@ -0,0 +1,80 @@
+using Gnocchi.Backend.API.Interfaces;
+using Gnocchi.Backend.App.DTOs;
+using Gnocchi.Backend.App.Interfaces;
+using Gnocchi.Backend.Models;
+
+namespace Gnocchi.Backend.App.Services;
+
+public class VariantService : IVariantService
+{
+    private readonly IVariantManager _variantManager;
+    public VariantService(IVariantManager variantManager)
+    {
+        _variantManager = variantManager;
+    }
+    public async Task<VariantDTO> AddVariantAsync(CreateVariantDTO createVariantDTO, CancellationToken ct = default)
+    {
+        Variant variant = new()
+        {
+            VariantId = Guid.NewGuid().ToString(),
+            Type = createVariantDTO.Type,
+            Dishes = createVariantDTO.Dishes
+
+        };
+
+        _variantManager.Add(variant);
+
+        return new VariantDTO
+        {
+            VariantId = variant.VariantId,
+            Type = variant.Type,
+            Dishes = variant.Dishes.ToList()
+        };
+    }
+
+    public async Task DeleteVariantAsync(DeleteVariantDTO deleteVariantDTO, CancellationToken ct = default)
+    {
+        var variant = await _variantManager.GetByIdAsync(deleteVariantDTO.VariantId, ct);
+
+        if (variant is null)
+        {
+            throw new NullReferenceException(message: "This ID does not belong to any variant!");
+        }
+
+        _variantManager.Remove(variant);
+    }
+
+    public async Task<IReadOnlyList<VariantDTO>> GetAllVariantsAsync(CancellationToken ct = default)
+    {
+        var variants = await _variantManager.GetAllAsync(ct);
+
+        if (variants is null)
+        {
+            return new List<VariantDTO>();
+        }
+
+        return variants.Select(variant => new VariantDTO
+        {
+            VariantId = variant.VariantId,
+            Type = variant.Type,
+            Dishes = variant.Dishes.ToList()
+        }).ToList();
+    }
+
+    public async Task<VariantDTO> GetVariantByIdAsync(string id, CancellationToken ct = default)
+    {
+        var variant = await _variantManager.GetByIdAsync(id, ct);
+
+        if (variant is null)
+        {
+            throw new NullReferenceException(message: "This ID does not belong to any Variant!");
+        }
+
+        return new VariantDTO
+        {
+            VariantId = variant.VariantId,
+            Type = variant.Type,
+            Dishes = variant.Dishes.ToList()
+        };
+    }
+}
