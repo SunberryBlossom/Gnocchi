27b3b6de09de6a70bc7d58362d51c754c8ef0bdb
SunberryBlossom <codemonke.lethargy455@passmail.com>
2026-09-07 16:38:29 +0200

    fix: handle broken naming convention of Rating property

--- Gnocchi.Backend/Gnocchi.Backend.App/DTOs/ScoreDTOs/CreateScoreDTO.cs
+++ Gnocchi.Backend/Gnocchi.Backend.App/DTOs/ScoreDTOs/CreateScoreDTO.cs
@@ -5,7 +5,7 @@ namespace Gnocchi.Backend.App.DTOs;
 
 public record CreateScoreDTO
 {
-    public Rating rating {get; set;}
+    public Rating Rating {get; set;}
     public ICollection<Dish> Dishes { get; set; } = new List<Dish>();
     public ICollection<CookingMethod> CookingMethods { get; set; } = new List<CookingMethod>();
     public ICollection<Ingredient> Ingredients { get; set; } = new List<Ingredient>();
