a88f2dae168b01ac32f8ed78094a1b73c22d7d7e
SunberryBlossom <codemonke.lethargy455@passmail.com>
2026-09-09 12:16:56 +0200

    feat!(API): reformat program.cs to asyn
    Due to the desire to have some seed data and have it running
    asynchronously, a small overhaul of program.cs has been made.
    
    In this, except for adding the seed generator, I've also added an
    exceptionhandler than returns HTTP codes mapped to the implemented
    exceptions.

--- Gnocchi.Backend/Gnocchi.Backend.API/Program.cs
+++ Gnocchi.Backend/Gnocchi.Backend.API/Program.cs
@@ -1,4 +1,5 @@
 using Gnocchi.Backend.API.Interfaces;
+using Gnocchi.Backend.API.Identity;
 using Gnocchi.Backend.App.Interfaces;
 using Gnocchi.Backend.App.Services;
 using Gnocchi.Backend.BLL.Interfaces;
@@ -14,13 +15,26 @@ namespace Gnocchi.Backend.API;
 
 public class Program
 {
-    public static void Main(string[] args)
+    public static async Task Main(string[] args)
     {
         #region Service container configuration
         var builder = WebApplication.CreateBuilder(args);
         builder.Services.AddDbContext<GnocchiDbContext>(options => options.UseSqlServer(builder.Configuration.GetConnectionString("DefaultConnection")));
+        builder.Services.AddHttpContextAccessor();
+        builder.Services.AddScoped<ICurrentUserAccessor, HttpCurrentUserAccessor>();
+        builder.Services.AddScoped<IUnitOfWork>(services => services.GetRequiredService<GnocchiDbContext>());
         builder.Services.AddIdentityApiEndpoints<User>(options => options.User.RequireUniqueEmail = true).AddRoles<IdentityRole>().AddEntityFrameworkStores<GnocchiDbContext>();
         builder.Services.AddControllers();
+        builder.Services.AddExceptionHandler(exceptionOptions =>
+        {
+            exceptionOptions.ExceptionHandler = async context =>
+            {
+                var exception = context.Features.Get<Microsoft.AspNetCore.Diagnostics.IExceptionHandlerFeature>()?.Error;
+                context.Response.StatusCode = exception is NullReferenceException ? StatusCodes.Status404NotFound : StatusCodes.Status500InternalServerError;
+                await context.Response.CompleteAsync();
+            };
+        });
+        builder.Services.AddProblemDetails();
         builder.Services.AddOpenApi();
         builder.Services.AddAuthorization();
         #region Repositories
@@ -53,6 +67,19 @@ public class Program
         #endregion
         #region Middleware configuration
         var app = builder.Build();
+        app.UseExceptionHandler();
+        if (app.Environment.IsDevelopment())
+        {
+            using var scope = app.Services.CreateScope();
+            try
+            {
+                await DevelopmentIdentitySeeder.SeedAsync(scope.ServiceProvider, builder.Configuration, app.Logger);
+            }
+            catch (Exception exception)
+            {
+                app.Logger.LogWarning(exception, "Could not provision development Identity seed data during startup.");
+            }
+        }
         if (app.Environment.IsDevelopment())
         {
             app.MapOpenApi();
@@ -63,7 +90,7 @@ public class Program
         app.UseAuthorization();
         app.MapIdentityApi<User>();
         app.MapControllers();
-        app.Run();
+        await app.RunAsync();
         #endregion
     }
 }
