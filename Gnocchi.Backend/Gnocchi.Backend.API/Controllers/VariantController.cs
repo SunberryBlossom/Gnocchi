67102e7624e09aaa14a1677bc829230e02ed4053
SunberryBlossom <codemonke.lethargy455@passmail.com>
2026-09-09 09:14:18 +0200

    feat(API): implement VariantController

---
+++ Gnocchi.Backend/Gnocchi.Backend.API/Controllers/VariantController.cs
@@ -0,0 +1,58 @@
+using Gnocchi.Backend.API.Interfaces;
+using Gnocchi.Backend.App.DTOs;
+using Microsoft.AspNetCore.Mvc;
+
+namespace Gnocchi.Backend.API.Controllers;
+
+[Route("api/[controller]")]
+[ApiController]
+public class VariantsController : ControllerBase
+{
+    private readonly IVariantService _variantService;
+    public VariantsController(IVariantService variantService)
+    {
+        _variantService = variantService;
+    }
+
+    [HttpGet]
+    public async Task<ActionResult<IReadOnlyList<VariantDTO>>> Get()
+    {
+        var variantDTOs = await _variantService.GetAllVariantsAsync();
+        if (!variantDTOs.Any())
+        {
+            return NotFound();
+        }
+
+        return Ok(variantDTOs);
+    }
+
+    [HttpGet]
+    [Route("{id:Guid}")]
+    public async Task<ActionResult<VariantDTO>> GetById([FromRoute]Guid id)
+    {
+        var variantDTO = await _variantService.GetVariantByIdAsync(id.ToString());
+
+        if(variantDTO is null)
+        {
+            return NotFound();
+        }
+        
+        return Ok(variantDTO);
+    }
+
+    [HttpPost]
+    public async Task<ActionResult<VariantDTO>> Post([FromBody]CreateVariantDTO DTO)
+    {
+        var result = await _variantService.AddVariantAsync(DTO);
+
+        return Created(result.VariantId, result);
+    }
+
+    [HttpDelete]
+    public async Task<IActionResult> Delete([FromBody]DeleteVariantDTO DTO)
+    {
+        await _variantService.DeleteVariantAsync(DTO);
+
+        return NoContent();
+    }
+}
