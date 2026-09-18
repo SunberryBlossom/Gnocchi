bcdfeb21e0adc37dbaf240bb0f180ee38a447092
SunberryBlossom <codemonke.lethargy455@passmail.com>
2026-09-18 16:14:27 +0200

    chore: final cleaning

--- Gnocchi.Backend/Gnocchi.Backend.Models/Models/Score.cs
+++ Gnocchi.Backend/Gnocchi.Backend.Models/Models/Score.cs
@@ -1,4 +1,8 @@
 using Gnocchi.Backend.Shared.Enums;
+using Microsoft.EntityFrameworkCore;
+using System.ComponentModel.DataAnnotations;
+using System.ComponentModel.DataAnnotations.Schema;
+
 namespace Gnocchi.Backend.Models;
 
 [Index(nameof(Rating))]
@@ -8,11 +12,12 @@ public class Score
     public string? ScoreId { get; set; }
     public Rating Rating { get; set; }
     #endregion
+
     #region Navigation properties
     [ForeignKey(nameof(User))]
-     public string? UserId { get; set; }
-    [Required(ErrorMessage = "A score must belong to a user."), DeleteBehavior(DeleteBehavior.Cascade)]
+    public string? UserId { get; set; }
     public User? User { get; set; }
+
     public ICollection<Dish> Dishes { get; set; } = new List<Dish>();
     public ICollection<CookingMethod> CookingMethods { get; set; } = new List<CookingMethod>();
     public ICollection<Ingredient> Ingredients { get; set; } = new List<Ingredient>();
